aef2287ff1b3febac833ae3a4f54aa0d3a06f062
btwitsmyname <brandontwiggins2000@gmail.com>
2024-08-07 14:09:37 -0400

    wait

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@ echo "NS3-Installation Script"
 mkdir NS3
 cd NS3
 wget https://www.nsnam.org/releases/ns-allinone-3.42.tar.bz2
+wait <wget https://www.nsnam.org/release/nsallinone-3.42.tar.bz2>
 cd ns-allinone-3.42/ns-3.42
 git clone https://gitlab.com/nsnam/ns-3-dev.git
+wait <git clone https:gitlab.com/nsnam/ns-3-dev.git>
 cd ns-3-dev
 ./ns3 configure --enable-examples --enable-tests\n
 ./ns3 build
